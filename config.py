de77365542d390a56c368f406b599a3abb2a9443
Simon Egorov <rhinorofl@gmail.com>
2023-10-10 00:15:00 +0700

    Update configs

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ dotenv.load_dotenv()
 
 
 class Config:
-    DB_NAME = os.getenv('DB_NAME', default='reksoft')
-    DB_USERNAME = os.getenv('DB_USERNAME', default='postgres')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', default='postgres')
-    DB_HOST = os.getenv('DB_HOST', default='localhost')
-    DB_PORT = os.getenv('DB_PORT', default=5432)
+    DB_NAME = os.getenv("DB_NAME", default="postgres")
+    DB_USERNAME = os.getenv("DB_USERNAME", default="postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", default="postgres")
+    DB_HOST = os.getenv("DB_HOST", default="localhost")
+    DB_PORT = os.getenv("DB_PORT", default=5432)
