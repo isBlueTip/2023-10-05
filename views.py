b891e844d10e4758d5f8e0c575c3915ef74f6471
Simon Egorov <rhinorofl@gmail.com>
2023-10-10 15:58:22 +0700

    Update views to handle lists

--- views.py
+++ views.py
@@ -1,7 +1,9 @@
 import dataclasses
 import json
 from abc import ABC, abstractmethod
+from typing import List
 
+import exceptions
 from db.models import Resource, ResourceType
 
 
@@ -12,18 +14,32 @@ class BaseSerializer(ABC):
 
 
 class ResourceTypeSerializer(BaseSerializer):
-    def __init__(self, obj: ResourceType):
-        self.obj = obj
+    def __init__(self, objs: ResourceType | List[ResourceType]):
+        self.objs = objs
 
     def serialize(self) -> str:
-        json_string = json.dumps(dataclasses.asdict(self.obj))
-        return json_string
+        if isinstance(self.objs, ResourceType):
+            json_string = json.dumps(dataclasses.asdict(self.objs))
+            return json_string
+        elif isinstance(self.objs, list) and all(isinstance(obj, ResourceType) for obj in self.objs):
+            json_list = [dataclasses.asdict(obj) for obj in self.objs]
+            json_string = json.dumps(json_list)
+            return json_string
+        print("ERROR: while jsoning data from controller")
+        raise exceptions.InternalServerError()
 
 
 class ResourceSerializer(BaseSerializer):
-    def __init__(self, obj: Resource):
-        self.obj = obj
+    def __init__(self, objs: Resource):
+        self.objs = objs
 
     def serialize(self) -> str:
-        json_string = json.dumps(dataclasses.asdict(self.obj))
-        return json_string
+        if isinstance(self.objs, Resource):
+            json_string = json.dumps(dataclasses.asdict(self.objs))
+            return json_string
+        elif isinstance(self.objs, list) and all(isinstance(obj, Resource) for obj in self.objs):
+            json_list = [dataclasses.asdict(obj) for obj in self.objs]
+            json_string = json.dumps(json_list)
+            return json_string
+        print("ERROR: while jsoning data from controller")
+        raise exceptions.InternalServerError()
